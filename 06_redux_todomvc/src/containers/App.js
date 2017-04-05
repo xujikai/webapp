a71505d457df5785d9ec60ee3d53fa8a907c1eaf
xujikai <smallwinterman@163.com>
2017-04-05 18:25:07 +0800

    添加redux-todomvc示例

---
+++ 06_redux_todomvc/src/containers/App.js
@@ -0,0 +1,32 @@
+/**
+ * Created by xxx on 2017/4/5.
+ */
+import React,{PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as TodoActions from '../actions';
+import Header from '../components/Header';
+import MainSection from '../components/MainSection';
+
+const App = ({todos,actions}) => (
+    <div>
+        <Header addTodo={actions.addTodo}/>
+        <MainSection todos={todos} actions={actions}/>
+    </div>
+);
+
+App.propTypes = {
+    todos: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(TodoActions,dispatch)
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
+
